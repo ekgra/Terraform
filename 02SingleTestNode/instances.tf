fc13402f943bca2abd78c05cc0dc7b9a0429d4a0
outlander <k2awsprod@gmail.com>
2023-10-22 16:49:25 +1100

    update instance resource name

--- 02SingleTestNode/instances.tf
+++ 02SingleTestNode/instances.tf
@@ -3,7 +3,7 @@
 #   name     = "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-20230517"
 # }
 
-data "aws_ami" "ubuntu-ami" {
+data "aws_ami" "machine-ami" {
   provider = aws.provider
   filter {
     name   = "image-id"
@@ -22,7 +22,7 @@ data "aws_key_pair" "sydneyKeyPair" {
 
 resource "aws_instance" "k-test-node" {
   provider                    = aws.provider
-  ami                         = data.aws_ami.ubuntu-ami.id
+  ami                         = data.aws_ami.machine-ami.id
   instance_type               = var.instance-type
   key_name                    = data.aws_key_pair.sydneyKeyPair.key_name
   associate_public_ip_address = true
@@ -32,7 +32,7 @@ resource "aws_instance" "k-test-node" {
   ebs_block_device {
     device_name = "/dev/xvdba"
     volume_type = "gp3"
-    volume_size = 30
+    volume_size = 15
   }
 
   root_block_device {
@@ -49,87 +49,3 @@ resource "aws_instance" "k-test-node" {
 }
 
 
-
-
-
-
-
-
-# resource "aws_instance" "k8s-master" {
-#   provider                    = aws.provider
-#   ami                         = data.aws_ami.ubuntu-ami.id
-#   instance_type               = var.instance_type
-#   key_name                    = data.aws_key_pair.sydneyKeyPair.key_name
-#   associate_public_ip_address = true
-#   vpc_security_group_ids      = [aws_security_group.k8s-sec-grp.id]
-#   subnet_id                   = aws_subnet.k8s-subnet.id
-#   ebs_block_device {
-#     device_name = "/dev/xvdba"
-#     volume_type = "gp3"
-#     volume_size = 30
-#   }
-#   root_block_device {
-#     volume_type = "gp3"
-#     volume_size = 10
-#   }
-
-#   user_data = data.template_file.init-master.rendered
-#   tags = {
-#     Name      = "k8s-node-0"
-#     owner     = "k Gaur"
-#     expire-on = "2023-12-31"
-#   }
-
-# }
-# resource "aws_instance" "k8s-worker-1" {
-#   provider                    = aws.provider
-#   ami                         = data.aws_ami.ubuntu-ami.id
-#   instance_type               = var.instance_type
-#   key_name                    = data.aws_key_pair.sydneyKeyPair.key_name
-#   associate_public_ip_address = true
-#   vpc_security_group_ids      = [aws_security_group.k8s-sec-grp.id]
-#   subnet_id                   = aws_subnet.k8s-subnet.id
-#   ebs_block_device {
-#     device_name = "/dev/xvdba"
-#     volume_type = "gp3"
-#     volume_size = 30
-#   }
-#   root_block_device {
-#     volume_type = "gp3"
-#     volume_size = 10
-#   }
-
-#   user_data = data.template_file.init-worker-1.rendered
-#   tags = {
-#     Name      = "k8s-node-1"
-#     owner     = "k Gaur"
-#     expire-on = "2023-12-31"
-#   }
-
-# }
-# resource "aws_instance" "k8s-worker-2" {
-#   provider                    = aws.provider
-#   ami                         = data.aws_ami.ubuntu-ami.id
-#   instance_type               = var.instance_type
-#   key_name                    = data.aws_key_pair.sydneyKeyPair.key_name
-#   associate_public_ip_address = true
-#   vpc_security_group_ids      = [aws_security_group.k8s-sec-grp.id]
-#   subnet_id                   = aws_subnet.k8s-subnet.id
-#   ebs_block_device {
-#     device_name = "/dev/xvdba"
-#     volume_type = "gp3"
-#     volume_size = 30
-#   }
-#   root_block_device {
-#     volume_type = "gp3"
-#     volume_size = 10
-#   }
-
-#   user_data = data.template_file.init-worker-2.rendered
-#   tags = {
-#     Name      = "k8s-node-2"
-#     owner     = "k Gaur"
-#     expire-on = "2023-12-31"
-#   }
-
-# }
